957f49c9606c4c8c0bbafefb9af7109effa9ece7
newne <ncgnewne@126.com>
2024-03-15 16:46:40 +0800

    svd拟合

--- main.py
+++ main.py
@@ -1,91 +1,120 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import plotly.graph_objs as go
-
-# matplotlib 图形正常显示中文及负号
-plt.rcParams["font.sans-serif"] = ["SimHei"]  # 用来正常显示中文标签
-plt.rcParams["axes.unicode_minus"] = False  # 用来正常显示负号
-# 二次曲面拟合/leastsq库的使用
-###最小二乘法试验###
-# error是自定义计算误差的函数，k,b也就是p0是计算初始化值，args是error其余的参数，该函数返回2个值，第一个是k,b的值
-from scipy.optimize import leastsq
-
 import numpy as np
-import matplotlib.pyplot as plt
-import cufflinks as cf
-
-cf.go_offline()
-
-
-# 拟合曲面函数
-def read_data(file_path):
-    data = pd.read_csv(file_path, header=[0])
-
-    # plt.show()
-    # 假设CSV文件中的数据格式为(x, y, z)
-    x = data.loc[:, "x"]
-    y = data.loc[:, "y"]
-    z = data.loc[:, "z"]
-    # trace = go.Surface(x=x, y=y, z=z)
-    # data = [trace]
-    # layout = go.Layout(title="3D Surface plot")
-    # fig = go.Figure(data=data)
-
-    return x, y, z
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
 
 
-# 绘制曲面图
-def plot_surface(X, Y, Z):
-    fig = plt.figure()
-    ax = fig.gca(projection="3d")
-    ax.plot_surface(X, Y, Z, cmap="viridis")
-    plt.show()
-
-
-# 主函数
 def main():
-    file_path = "coordinates.csv"  # 替换为你的CSV文件路径
-    X, Y, Z = read_data(file_path)
-    # TEST
-    plot_mesh(X, Y, Z)
-    p0 = [1, 1, 1, 1, 1, 1]
+    st.set_page_config(
+        page_title="平面拟合",
+        page_icon="",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+        menu_items={"About": "Wei Qian copyright",},
+    )
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        st.write("filename:", uploaded_file.name)
+        df = pd.read_csv(uploaded_file)
+        df.columns = ["x", "y", "z"]
+        st.dataframe(df, use_container_width=True)
+        if df.empty:
+            st.warning("No data found")
+            return
+        points = df.values
 
-    # print( error(p0,Xi,Yi) )
+        # 计算点云的质心
+        centroid = np.mean(points, axis=0)
 
-    ###主函数从此开始###
-    s = "Test the number of iteration"  # 试验最小二乘法函数leastsq得调用几次error函数才能找到使得均方误差之和最小的k、b
-    Para = leastsq(error, np.array(p0), args=(X, Y, Z))  # 把error函数中除了p以外的参数打包到args中
-    # a, b, c, d, e, f = Para[0]
-    print("根据数据点，得到函数f(x,y)=ax^2+by^2+cxy+dx+ey+f的拟合系数依次为：".format(Para[0]))
-    # print(Para[0])
+        # 将点云的质心平移到原点
+        points_centered = points - centroid
 
-    Z1 = func(X, Y, Para[0])
-    plot_mesh(X, Y, Z1)
-    ###绘图，看拟合效果###
+        # 使用SVD分解质心化后的点云矩阵
+        U, S, Vt = np.linalg.svd(points_centered)
+
+        # 平面的法向量是Vt矩阵的最后一列
+        normal = Vt[-1, :]
+
+        # 确保法向量指向z轴正方向
+        if normal[2] < 0:
+            normal = -normal
+
+        # 平面方程：ax + by + cz = d
+        # 其中(a, b, c)是法向量，d是原点到平面的距离
+        a, b, c = normal
+
+        d = -np.dot(normal, centroid)
+        print("平面拟合结果为： %.3f * x + %.3f * y + %.3f*z + %3.f=0" % (a, b, c, d))
+        fig = draw_plane(a, b, c, d, points)
+        st.write("平面拟合结果为： %.3f * x + %.3f * y + %.3f*z + %3.f=0 " % (a, b, c, d))
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        pred = pd.read_csv("data/predict.csv")
+        pred.columns = ["x", "y"]
+        pred["z"] = pred.apply(cal_z, args=(a, b, c, d,), axis=1)
+        st.dataframe(pred, use_container_width=True)
+        st.write("this is pred:")
+        fig = draw_plane(a, b, c, d, pred.values)
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
-def plot_mesh(X, Y, Z):
-    # 使用Seaborn绘制三维曲面图
-    fig = plt.figure(figsize=(10, 7))
-    ax = fig.add_subplot(111, projection="3d")
-    # 创建曲面图
-    surf = ax.plot_trisurf(X, Y, Z, cmap="viridis", edgecolor="none")
-    # 设置图表标题和轴标签
-    ax.set_title("3D Surface Plot")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    # 添加颜色条
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.show()
+def draw_plane(a, b, c, d, points):
+    # 创建平面网格
+    x_range = np.linspace(np.min(points[:, 0]), np.max(points[:, 0]), 30)
+    y_range = np.linspace(np.min(points[:, 1]), np.max(points[:, 1]), 30)
+    x_grid, y_grid = np.meshgrid(x_range, y_range)
+    z_grid = (-d - a * x_grid - b * y_grid) / c
+    # 绘制点和拟合的平面
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=points[:, 0],
+                y=points[:, 1],
+                z=points[:, 2],
+                mode="markers",
+                marker=dict(size=5),
+                name="Points",
+            ),
+            go.Surface(
+                x=x_grid,
+                y=y_grid,
+                z=z_grid,
+                opacity=0.5,
+                showscale=False,
+                name="Fitted Plane",
+            ),
+        ]
+    )
+    # 设置图形布局
+    # 设置图形布局
+    fig.update_layout(
+        width=800,
+        height=450,
+        autosize=False,
+        margin=dict(l=65, r=50, b=65, t=90),
+        title="3D Scatter Plot with Plane Equation",
+        scene=dict(
+            xaxis_title="X",
+            yaxis_title="Y",
+            zaxis_title="Z",
+            annotations=[
+                dict(
+                    showarrow=False,
+                    x=0,
+                    y=0,
+                    z=0,
+                    text=f"Plane Equation: {a:.4f}x + {b:.4f}y + {c:.4f}z = {d:.4f}",
+                    xanchor="left",
+                    xshift=10,
+                    opacity=0.7,
+                )
+            ],
+        ),
+    )
+    return fig
 
 
-def func(x, y, p):
-    return p[0] * x ** 2 + p[1] * y ** 2 + p[2] * x * y + p[3] * x + p[4] * y + p[5]
-
-
-def error(p, x, y, z):
-    return abs(func(x, y, p) - z)  # x、y都是列表，故返回值也是个列表
+def cal_z(row, a, b, c, d):
+    return (-d - a * row["x"] - b * row["y"]) / c
 
 
 if __name__ == "__main__":
--- svd_plane_fit.py
+++ svd_plane_fit.py
@@ -1,103 +1,103 @@
-import numpy as np
-
-from pclpy import pcl
-
-
-def plane(cloud, normal_vector):
-    coeffs = pcl.ModelCoefficients()  # 创建了一个模型系数对象
-    coeffs.values.append(normal_vector[0])  # a = 0.0
-    coeffs.values.append(normal_vector[1])  # b = 0.0
-    coeffs.values.append(normal_vector[2])  # c = 1.0
-    coeffs.values.append(normal_vector[3])  # d = 0.0
-
-    # 创建滤波器
-    proj = pcl.filters.ProjectInliers.PointXYZ()  # 过滤器对象 proj，用于将点云投影到一个模型上。
-    proj.setModelType(0)  # 模型类型被设为 0，代表使用平面模型。
-    proj.setInputCloud(cloud)  # 将cloud点云数据进行处理
-    proj.setModelCoefficients(coeffs)  # 处理参数coeffs
-    cloud_projected = pcl.PointCloud.PointXYZ()  # 建立保存点云
-    proj.filter(cloud_projected)  # 将投影结果保存
-
-    return cloud_projected
-def compareCloudShow(cloud1, cloud2):
-    """
-    Args:在一个窗口生成2个窗口可视化点云
-        cloud1: 点云数据1
-        cloud2: 点云数据2
-    """
-    viewer = pcl.visualization.PCLVisualizer("viewer")  # 建立可刷窗口对象 窗口名 viewer
-    v0 = 1  # 设置标签名（0, 1标记第一个窗口）
-    viewer.createViewPort(0.0, 0.0, 0.5, 1.0, v0)  # 创建一个可视化的窗口
-    viewer.setBackgroundColor(0.0, 0.0, 0.0, v0)  # 设置窗口背景为黑色
-    single_color = pcl.visualization.PointCloudColorHandlerCustom.PointXYZ(cloud1, 255.0, 0, 0.0)  # 将点云设置为红色
-    viewer.addPointCloud(cloud1,          # 要添加到窗口的点云数据。
-                         single_color,    # 指定点云的颜色
-                         "sample cloud1",  # 添加的点云命名
-                         v0)  # 点云添加到的视图
-
-    v1 = 2  # 设置标签名（2代表第二个窗口）
-    viewer.createViewPort(0.5, 0.0, 1.0, 1.0, v1)  # 创建一个可视化的窗口
-    viewer.setBackgroundColor(255.0, 255.0, 255.0, v1)  # 设置窗口背景为白色
-    single_color = pcl.visualization.PointCloudColorHandlerCustom.PointXYZ(cloud2, 0.0, 255.0, 0.0)  # 将点云设置为绿色
-    viewer.addPointCloud(cloud2,  # 要添加到窗口的点云数据。
-                         single_color,  # 指定点云的颜色
-                         "sample cloud2",  # 添加的点云命名
-                         v1)  # 点云添加到的视图
-
-    # 设置点云窗口（可移除对点云可视化没有影响）
-    viewer.setPointCloudRenderingProperties(0,  # 设置点云点的大小
-                                            1,  # 点云像素
-                                            "sample cloud1",  # 识别特定点云
-                                            v0)  # 在那个窗口可视化
-    viewer.setPointCloudRenderingProperties(0,  # 设置点云点的大小
-                                            1,  # 点云像素
-                                            "sample cloud2",  # 识别特定点云
-                                            v1)  # 在那个窗口可视化
-    # viewer.addCoordinateSystem(1.0)  # 设置坐标轴 坐标轴的长度为1.0
-    # 窗口建立
-    while not viewer.wasStopped():
-        viewer.spinOnce(10)
-
-if __name__ == '__main__':
-    """ 拟合出的平面方程为： ax + by +cz + d = 0"""
-    """约束条件 a^2 + b^2 + c^2 = 1"""
-    cloud1 = pcl.PointCloud.PointXYZ()
-    reader = pcl.io.PCDReader()  # 设置读取对象
-    reader.read('res/bunny.pcd', cloud1)  # 读取点云保存在cloud中
-    points = cloud1.xyz
-    # 获得平均值
-    avg_x = np.mean(points[:, 0])
-    avg_y = np.mean(points[:, 1])
-    avg_z = np.mean(points[:, 2])
-
-    # 去重心化
-    x = points[:, 0] - avg_x
-    y = points[:, 1] - avg_y
-    z = points[:, 2] - avg_z
-    # 生成矩阵
-    A = np.transpose(np.asarray([x, y, z]))  # 将数据矩阵化
-
-    """AX = 0"""
-    # 计算协方差矩阵
-    cov_matrix = np.cov(A, rowvar=False)
-
-    # 使用 SVD 分解求解协方差矩阵的特征值和特征向量
-    U, S, Vt = np.linalg.svd(cov_matrix)
-
-    # 特征值和特征向量
-    eigenvalues = S
-    eigenvectors = Vt.T  # 转置后得到正确的特征向量 一行代表一个特征向量 最小的特征向量在最后一行
-
-    print("特征值：", eigenvalues)
-    print("特征向量：", eigenvectors)
-
-    """如果想要找到最小的特征向量，可以直接从特征向量矩阵中取最后一列（没有转置前），
-    因为特征向量是按特征值从大到小排列的，所以最后一列就是对应最小特征值的特征向量。"""
-    X = eigenvectors[2]
-    center =  np.mean(points, axis=0)  # 获得中心（质心）质心是一个几何概念，通常用于描述几何图形（如点集、线段、多边形等）的中心位置。
-    print('平面拟合结果为：0 = %.3f * x + %.3f * y + %.3f + %3.f' % (
-    X[0], X[1], X[2], (-X[0] * center[0] - X[1] * center[1] - X[2] * center[2])))
-    plane_cloud = plane(cloud1,
-                        (X[0], X[1], X[2], (-X[0] * center[0] - X[1] * center[1] - X[2] * center[2])))  # 获得投影后的点云数据
-    # ------------------ 可视化点云 -----------------
-    compareCloudShow(cloud1, plane_cloud)
+# import numpy as np
+#
+# from pclpy import pcl
+#
+#
+# def plane(cloud, normal_vector):
+#     coeffs = pcl.ModelCoefficients()  # 创建了一个模型系数对象
+#     coeffs.values.append(normal_vector[0])  # a = 0.0
+#     coeffs.values.append(normal_vector[1])  # b = 0.0
+#     coeffs.values.append(normal_vector[2])  # c = 1.0
+#     coeffs.values.append(normal_vector[3])  # d = 0.0
+#
+#     # 创建滤波器
+#     proj = pcl.filters.ProjectInliers.PointXYZ()  # 过滤器对象 proj，用于将点云投影到一个模型上。
+#     proj.setModelType(0)  # 模型类型被设为 0，代表使用平面模型。
+#     proj.setInputCloud(cloud)  # 将cloud点云数据进行处理
+#     proj.setModelCoefficients(coeffs)  # 处理参数coeffs
+#     cloud_projected = pcl.PointCloud.PointXYZ()  # 建立保存点云
+#     proj.filter(cloud_projected)  # 将投影结果保存
+#
+#     return cloud_projected
+# def compareCloudShow(cloud1, cloud2):
+#     """
+#     Args:在一个窗口生成2个窗口可视化点云
+#         cloud1: 点云数据1
+#         cloud2: 点云数据2
+#     """
+#     viewer = pcl.visualization.PCLVisualizer("viewer")  # 建立可刷窗口对象 窗口名 viewer
+#     v0 = 1  # 设置标签名（0, 1标记第一个窗口）
+#     viewer.createViewPort(0.0, 0.0, 0.5, 1.0, v0)  # 创建一个可视化的窗口
+#     viewer.setBackgroundColor(0.0, 0.0, 0.0, v0)  # 设置窗口背景为黑色
+#     single_color = pcl.visualization.PointCloudColorHandlerCustom.PointXYZ(cloud1, 255.0, 0, 0.0)  # 将点云设置为红色
+#     viewer.addPointCloud(cloud1,          # 要添加到窗口的点云数据。
+#                          single_color,    # 指定点云的颜色
+#                          "sample cloud1",  # 添加的点云命名
+#                          v0)  # 点云添加到的视图
+#
+#     v1 = 2  # 设置标签名（2代表第二个窗口）
+#     viewer.createViewPort(0.5, 0.0, 1.0, 1.0, v1)  # 创建一个可视化的窗口
+#     viewer.setBackgroundColor(255.0, 255.0, 255.0, v1)  # 设置窗口背景为白色
+#     single_color = pcl.visualization.PointCloudColorHandlerCustom.PointXYZ(cloud2, 0.0, 255.0, 0.0)  # 将点云设置为绿色
+#     viewer.addPointCloud(cloud2,  # 要添加到窗口的点云数据。
+#                          single_color,  # 指定点云的颜色
+#                          "sample cloud2",  # 添加的点云命名
+#                          v1)  # 点云添加到的视图
+#
+#     # 设置点云窗口（可移除对点云可视化没有影响）
+#     viewer.setPointCloudRenderingProperties(0,  # 设置点云点的大小
+#                                             1,  # 点云像素
+#                                             "sample cloud1",  # 识别特定点云
+#                                             v0)  # 在那个窗口可视化
+#     viewer.setPointCloudRenderingProperties(0,  # 设置点云点的大小
+#                                             1,  # 点云像素
+#                                             "sample cloud2",  # 识别特定点云
+#                                             v1)  # 在那个窗口可视化
+#     # viewer.addCoordinateSystem(1.0)  # 设置坐标轴 坐标轴的长度为1.0
+#     # 窗口建立
+#     while not viewer.wasStopped():
+#         viewer.spinOnce(10)
+#
+# if __name__ == '__main__':
+#     """ 拟合出的平面方程为： ax + by +cz + d = 0"""
+#     """约束条件 a^2 + b^2 + c^2 = 1"""
+#     cloud1 = pcl.PointCloud.PointXYZ()
+#     reader = pcl.io.PCDReader()  # 设置读取对象
+#     reader.read('res/bunny.pcd', cloud1)  # 读取点云保存在cloud中
+#     points = cloud1.xyz
+#     # 获得平均值
+#     avg_x = np.mean(points[:, 0])
+#     avg_y = np.mean(points[:, 1])
+#     avg_z = np.mean(points[:, 2])
+#
+#     # 去重心化
+#     x = points[:, 0] - avg_x
+#     y = points[:, 1] - avg_y
+#     z = points[:, 2] - avg_z
+#     # 生成矩阵
+#     A = np.transpose(np.asarray([x, y, z]))  # 将数据矩阵化
+#
+#     """AX = 0"""
+#     # 计算协方差矩阵
+#     cov_matrix = np.cov(A, rowvar=False)
+#
+#     # 使用 SVD 分解求解协方差矩阵的特征值和特征向量
+#     U, S, Vt = np.linalg.svd(cov_matrix)
+#
+#     # 特征值和特征向量
+#     eigenvalues = S
+#     eigenvectors = Vt.T  # 转置后得到正确的特征向量 一行代表一个特征向量 最小的特征向量在最后一行
+#
+#     print("特征值：", eigenvalues)
+#     print("特征向量：", eigenvectors)
+#
+#     """如果想要找到最小的特征向量，可以直接从特征向量矩阵中取最后一列（没有转置前），
+#     因为特征向量是按特征值从大到小排列的，所以最后一列就是对应最小特征值的特征向量。"""
+#     X = eigenvectors[2]
+#     center =  np.mean(points, axis=0)  # 获得中心（质心）质心是一个几何概念，通常用于描述几何图形（如点集、线段、多边形等）的中心位置。
+#     print('平面拟合结果为：0 = %.3f * x + %.3f * y + %.3f + %3.f' % (
+#     X[0], X[1], X[2], (-X[0] * center[0] - X[1] * center[1] - X[2] * center[2])))
+#     plane_cloud = plane(cloud1,
+#                         (X[0], X[1], X[2], (-X[0] * center[0] - X[1] * center[1] - X[2] * center[2])))  # 获得投影后的点云数据
+#     # ------------------ 可视化点云 -----------------
+#     compareCloudShow(cloud1, plane_cloud)
